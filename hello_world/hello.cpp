9603c002e425f55b446e7a27653c031750345e66
conan <242211@qq.com>
2023-06-29 18:17:18 +0800

    hello

---
+++ hello_world/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "hello world !" <<endl;
+    cout << "first line\n";          //\n表示新起一行
+    cout << "second line" << endl;   //endl表示新起一行
+    cin.get();                       //程序执行完毕后输入enter退出
+    return 0;
+}
---
+++ sqrt/sqrt.cpp
@@ -0,0 +1,10 @@
+//计算平方根
+#include <iostream>
+#include <cmath>  //或math.h
+
+using namespace std;
+
+int main()
+{
+
+}
