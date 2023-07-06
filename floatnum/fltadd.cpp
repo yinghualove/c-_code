d65570a8d9f14e080f13af3160b01341efb0499b
conan <242211@qq.com>
2023-07-06 18:24:47 +0800

    floatnum

---
+++ floatnum/fltadd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cfloat>
+
+using namespace std;
+
+int main()
+{
+    float a = 2.34E+22f;
+    float b = a + 1.0f;
+
+    cout << "a = " << a << endl;
+    cout << "b - a = "<< b-a << endl;
+    return 0;
+}
