e065c940b86dea299c955a47ddd3b1e755df705e
conan <242211@qq.com>
2023-08-28 15:00:00 +0800

    sqrt-v1.0

--- sqrt/sqrt.cpp
+++ sqrt/sqrt.cpp
@@ -6,5 +6,14 @@ using namespace std;
 
 int main()
 {
-
+    double area;
+    cout << "Enter the floor area, in square feet , of your home:";
+    cin >> area;
+    double side;
+    side = sqrt(area);
+    cout << "That's the equivalent of a square " << side
+    << " feet to the side." << endl;
+    cout << "How facinating !" << endl;
+    
+    return 0;
 }
